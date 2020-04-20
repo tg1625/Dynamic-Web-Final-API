e98c8bfde1bff4600b204afbf78c150cdf0537b7
tg1625 <35944491+tg1625@users.noreply.github.com>
2020-04-19 20:52:42 -0400

    add firebase config

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-const express = require('express') //using express
-const app = express() //making an app
-const port = 4000 //setting a port
+/*----- FIREBASE ------ */
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase");
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDq0kNN6_mua4b68uQNq9eVB7UMZsTxzjc",
+    authDomain: "final-project-f0e1e.firebaseapp.com",
+    databaseURL: "https://final-project-f0e1e.firebaseio.com",
+    projectId: "final-project-f0e1e",
+    storageBucket: "final-project-f0e1e.appspot.com",
+    messagingSenderId: "697304196124",
+    appId: "1:697304196124:web:1b4cf713fd36b0c59debd9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-/*-- Importing routing files --*/
-const indexRoute = require('./routes/index.js'); //like an IMPORT in react
+/* ---- EXPRESS --- */
+//import express
+const express = require('express')
+//initiate express to app
+const app = express()
+//set port, use environmental variables, or use 4000 if it doesn't exist 
+const port = process.env.PORT || 4000;
+
+//Import routes
+const indexRoute = require("./routes/index.js");
+// const postRoute = require("./routes/post.js");
+// const createRoute = require("./routes/createArticle.js");
+//Create routes 
+app.use('/', indexRoute);
+// app.use('/post', postRoute);
+// app.use('/create',createRoute);
 
 
-/*---- Serve Static Files ------*/
-// app.use(express.static('public'))
-app.use('/static', express.static('public'))  //this creates a virtual path, so images would be static/images instead of just images 
-
-/* ---- Routing in Express ----*/
-//format --> app('route', callback function)
-app.use('/', indexRoute)
-
-app.listen(port, () => console.log(`Hello World running on port ${port}!`))
+//Set up app to run
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
